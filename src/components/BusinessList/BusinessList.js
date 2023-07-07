74d8b99245eb20bfacf0241efc12be328c9c39be
panida-pov <panida.pov@gmail.com>
2023-07-07 16:49:37 +0900

    Update Business unique key

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,7 +7,7 @@ const BusinessList = ({businesses}) => {
   return (
     <div className={styles.BusinessList}>
       {businesses.length >  0 ? businesses.map(business => {
-        return <Business business={business} key={business.name}/>;
+        return <Business business={business} key={business.id}/>;
       }): <p className={styles.resultNotFound}>SORRY <i className="bi bi-emoji-frown"></i><br></br>RESULTS NOT FOUND</p>}
     </div>
   )
